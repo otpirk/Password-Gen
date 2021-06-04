0e95b5efde8a6883229c03fda1d52ef4713c9942
Cyber Knight <82527627+Cyber-Knight-Official@users.noreply.github.com>
2021-06-05 00:30:22 +0530

    Update PassGen.sh

--- PassGen.sh
+++ PassGen.sh
@@ -1,20 +1,51 @@
 
+#bash_colour_codes
+
+r= \e[1;31m 
+
+g= \e[1;32m 
+
+y= \e[1;33m 
+
+b= \e[1;34m 
+
+p= \e[1;35m 
+
+lb= \e[1;36m
+
 clear
+
 sleep 0.5
+
 toilet -f mono12  CKO | lolcat
+
 sleep 0.7
+
 clear 
 
 clear
+
 sleep 0.5
+
 toilet -f mono12  Password Gen | lolcat
+
 printf "\n"
+
 printf "            \e[1;31m\e[0m\e[1;92m Tool By\e[0m\e[1;77m  Cyber Knight Official \e[0m\e[1;31m\e[0m\n"
+
 printf "\n" 
+
 read -p $'\e[1;31m[\e[0m\e[1;77m+\e[0m\e[1;31m]\e[0m\e[1;32m Enter Youer Password Create Size : \e[0m' token
+
 printf "\n"
+
 printf "\e[1;33m Password Genarate Sucess !!! \e[0m\e[1;33m\e[0m\n"
+
 echo -e "\e[1;34m"
+
 </dev/urandom tr -dc '1234567890!@#$%qwertQWERTasdfgASDFGzxcvbZXCVB' | head -c$token; echo ""
+
 printf "\n"
+
 printf "\e[1;31m This Is Youer Password  \e[0m\e[1;31m\e[0m\n"
+
